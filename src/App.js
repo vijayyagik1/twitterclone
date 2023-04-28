e46aedceaccf857e8822b751ba9d9482f38e8773
AmanDsan <dubeyaman063@gmail.com>
2023-04-29 01:56:53 +0530

    segmented parts of project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useEffect, useRef } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { fetchTweets } from "./services/tweets";
+import { fetchUsers } from "./services/users";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { tweetsAtom } from "./recoil/tweets";
+import { authAtom, usersAtom } from "./recoil/users";
+import { log } from "joi-browser";
+
+const PR = ({ children }) => {
+  const navigate = useNavigate();
+  const [auth, setAuth] = useRecoilState(authAtom);
+  useEffect(() => {
+    console.log("First");
+    if (!auth?.isLoggedIn) {
+      navigate("/login");
+      console.log("navigating FormPR");
+    }
+  }, [auth]);
+
+  return children;
+};
 
 function App() {
+  const setTweets = useSetRecoilState(tweetsAtom);
+  const setUsers = useSetRecoilState(usersAtom);
+  const [auth, setAuth] = useRecoilState(authAtom);
+  const navigate = useNavigate();
+
+  const isFirstRender = useRef(true);
+
+  useEffect(() => {
+    console.log("second render");
+    if (isFirstRender.current) {
+      const authFromLocal = JSON.parse(localStorage.getItem("auth"));
+      setAuth(authFromLocal);
+    } else {
+      localStorage.setItem("auth", JSON.stringify(auth));
+    }
+
+    isFirstRender.current = false;
+  }, [auth]);
+
+  useEffect(() => {
+    fetchTweets().then((tweets) => {
+      setTweets(tweets);
+    });
+    fetchUsers().then((users) => {
+      setUsers(users);
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PR>
+              <HomePage />
+            </PR>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/register"
+          element={
+            <PR>
+              <Register />
+            </PR>
+          }
+        />
+      </Routes>
+    </Fragment>
   );
 }
 
