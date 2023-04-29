de14648ab95c47f1878d7ff3a6dae0e68d76ed4f
vijayyagik1 <126654908+vijayyagik1@users.noreply.github.com>
2023-04-29 16:52:17 +0530

    second

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+import WhoToFollow from "./assets/whotofollow/whotofollow"
+function App() {
+  return (
+    <div>
+      <WhoToFollow/>
+    </div>
+  )
+}
+
+export default App
